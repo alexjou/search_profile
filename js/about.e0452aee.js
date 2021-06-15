bef07d751385926fb80d027f326157c2e12d4b5b
alexjou <joubert2006@hotmail.com>
2021-06-15 03:05:29 -0300

    Github Pages

---
+++ js/about.e0452aee.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var a=n("7a23"),c={class:"about"};function o(t,e,n,o,r,s){var b=Object(a["u"])("Navbar");return Object(a["p"])(),Object(a["d"])("div",c,[Object(a["g"])(b),Object(a["g"])("h1",null,Object(a["w"])(n.msg),1)])}var r=n("d178"),s={name:"About",components:{Navbar:r["a"]},props:{msg:String},created:function(){console.log(this.msg)}};s.render=o;e["default"]=s}}]);
+//# sourceMappingURL=about.e0452aee.js.map
