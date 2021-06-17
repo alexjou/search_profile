6ed3652c8c408f7c6ba09a1d8fb4dab646fd7a1c
alexjou <joubert2006@hotmail.com>
2021-06-17 03:08:30 -0300

    Fix

--- src/components/Repo.vue
+++ src/components/Repo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card mb-2 w-2/3 sm:w-full sm:flex sm:items-center sm:justify-center sm:text-center">
+  <div class="card card-body mb-2 w-2/3 sm:hidden">
     <div class="flex flex-row sm:hidden">
       <div class="col-md-6 sm:col-md-1">
         <a :href="repo.html_url" target="_blank">{{ repo.name }}</a>
@@ -15,8 +15,10 @@
         </span>
         <span class="badge bg-green-600">Forks: {{ repo.forks_count }} </span>
       </div>
-    </div>
-    <div class="hidden sm:flex sm:flex-col">
+    </div>    
+  </div>
+  <div class="hidden card mb-2 sm:w-full sm:flex sm:items-center sm:justify-center sm:text-center">
+  <div class="flex flex-col">
         <a :href="repo.html_url" target="_blank">{{ repo.name }}</a>
         <h2>{{ repo.description }}</h2>
         <span class="badge bg-blue-600">Linguagem: {{ repo.language }} </span>
@@ -28,7 +30,7 @@
         </span>
         <span class="badge bg-green-600 mt-2">Forks: {{ repo.forks_count }} </span>
       </div>
-  </div>
+      </div>
 </template>
 
 <script>
